2fa9773191061dff6a0bdce6dbd9cebc81d08d6a
leonardodossantoszup <leonardo.dossantos@zup.com.br>
2023-08-31 08:59:37 -0300

    push

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,3 +1,6 @@
 #!/bin/sh -l
 
-/app/stackspot-runtime-job-iac start --input-path=${{ inputs.input_path }} >> $GITHUB_OUTPUT
+docker run --rm \
+-e FEATURES_LEVEL_LOG="$1" \
+--entrypoint=/app/stackspot-runtime-job-iac \
+stackspot/runtime-iac start --input-path="$2"
